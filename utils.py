e49c5711d850b147a6d9b90db7c224dd5d2df759
Ubuntu <ubuntu@ip-172-31-2-179.ec2.internal>
2018-07-26 03:57:24 +0000

    Update

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 import bcolz
+from skimage.transform import resize
 
 OR_IM_WIDTH = 101
 OR_IM_HEIGHT = 101
-OR_IM_CHANNEL = 101
+OR_IM_CHANNEL = 3
 
 IM_WIDTH = 128
 IM_HEIGHT = 128
@@ -16,3 +17,12 @@ def save_arr (fname, arr):
 
 def load_array(fname):
     return bcolz.open(fname)[:]
+
+
+def upsample(img):
+    return resize(img, (IM_HEIGHT, IM_WIDTH, IM_CHAN), mode='constant', preserve_range=True, anti_aliasing=True)
+    
+    
+def downsample(img):
+    return resize(img, (OR_IM_HEIGHT, OR_IM_WIDTH, OR_IM_CHANNEL), mode='constant', preserve_range=True, anti_aliasing=True)
+    
